c52749fe4eab31d0d43b1fa6409d6bdab2e6acf0
Dong Min Seol <50672087+SightStudio@users.noreply.github.com>
2019-09-14 03:19:06 +0900

    [2019.09.14] init commit
    시작 커밋
    
    현재까지 프론트엔드만 페이지 제작중이고, 백엔드는 템플릿만 존재함

---
+++ frontend/src/util/enum/enum.js
@@ -0,0 +1,13 @@
+
+/**
+ * 유저 권한 enum 
+ * - value 순으로
+ * 1. 총 관리자
+ * 2. 수입 업자
+ * 3. 유통 업자
+ * 4. 카페
+ * 
+ * @author Dong-Min Seol
+ * @since  2019.09.09
+ */
+export const AUTH = Object.freeze({'admin': 1, 'importer': 2, 'deliver': 3, 'cafe': 4,})
